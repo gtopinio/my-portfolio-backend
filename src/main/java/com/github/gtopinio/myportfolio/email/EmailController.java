965adf0e61e784a5fe864302b52dea03bdc4255c
gtopinio <mctopinio@up.edu.ph>
2023-07-23 01:52:00 +0800

    update: add mutation for saving email

--- src/main/java/com/github/gtopinio/myportfolio/email/EmailController.java
+++ src/main/java/com/github/gtopinio/myportfolio/email/EmailController.java
@@ -2,6 +2,7 @@ package com.github.gtopinio.myportfolio.email;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 
@@ -26,6 +27,14 @@ public class EmailController {
     @QueryMapping
     Optional<Email> emailById(@Argument Long id) {
         return this.emailService.getEmailById(id);
+    }
+
+    @MutationMapping
+    Email saveEmail(@Argument EmailInput email) {
+        return this.emailService.saveEmail(email.senderEmail, email.senderName, email.message);
+    }
+
+    record EmailInput(String senderEmail, String senderName, String message) {
     }
 
 }
--- src/main/java/com/github/gtopinio/myportfolio/email/EmailService.java
+++ src/main/java/com/github/gtopinio/myportfolio/email/EmailService.java
@@ -3,6 +3,7 @@ package com.github.gtopinio.myportfolio.email;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
@@ -23,5 +24,14 @@ public class EmailService {
 
     public Optional<Email> getEmailById(Long id) {
         return emailRepository.findById(id);
+    }
+
+    public Email saveEmail(String senderEmail, String senderName, String message) {
+        Email email = new Email();
+        email.setSenderEmail(senderEmail);
+        email.setSenderName(senderName);
+        email.setMessage(message);
+        email.setDate(LocalDate.now());
+        return emailRepository.save(email);
     }
 }
