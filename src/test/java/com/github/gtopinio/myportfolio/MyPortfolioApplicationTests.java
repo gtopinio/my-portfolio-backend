4d762869e93e87c90da83830e86fd13e2b663c15
gtopinio <mctopinio@up.edu.ph>
2023-07-22 21:32:03 +0800

    release: initial commit

---
+++ src/test/java/com/github/gtopinio/myportfolio/MyPortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.gtopinio.myportfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyPortfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
