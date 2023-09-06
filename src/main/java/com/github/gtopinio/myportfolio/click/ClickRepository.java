00b44384753a8c34e2a6c53aa23ec1c3a27dee06
gtopinio <mctopinio@up.edu.ph>
2023-09-06 21:43:09 +0800

    fix: add repository decorator in click repository interface

--- src/main/java/com/github/gtopinio/myportfolio/click/ClickRepository.java
+++ src/main/java/com/github/gtopinio/myportfolio/click/ClickRepository.java
@@ -1,7 +1,9 @@
 package com.github.gtopinio.myportfolio.click;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ClickRepository extends JpaRepository<Click, Long> {
     // Responsible for database access
 }
